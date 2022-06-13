180a1e5b26c10735489d62e5304ef1eedcdd4917
Elijah Chinonso <107347041+ElijahChinonso@users.noreply.github.com>
2022-06-13 15:39:57 +0100

    Add files via upload
    My Dog as a code

---
+++ Dog.java
@@ -0,0 +1,16 @@
+public class Dog{
+	private String head;
+	private String eyes;
+	private String nose;
+	private int age;
+		public Dog(){}
+	public Dog(String head, String eyes, String nose, int age){
+		this.head = head;
+		this.eyes = eyes;
+		this.nose = nose;
+		this.age = age;
+		}
+	public static void main(String[] args){
+	System.out.println("My Dog");
+	}
+}
