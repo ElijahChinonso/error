0be774af47f6a36c7a6a4163cc53b846789c52ea
Elijah Chinonso <107347041+ElijahChinonso@users.noreply.github.com>
2022-06-15 21:56:32 +0100

    Add files via upload
    final part of an oop

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		Pen p = new Pen();
+		System.out.println(p.color);
+		System.out.println(p.point);
+		System.out.println(p.point);
+		System.out.println(p.clicked);
+
+		p.click();
+
+		System.out.println(p.clicked);
+		}
+}
