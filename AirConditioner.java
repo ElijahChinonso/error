32ad8491e8a44536914e31f6231b0ee42db13bd7
Elijah Chinonso <107347041+ElijahChinonso@users.noreply.github.com>
2022-06-16 22:41:49 +0100

    Add files via upload
    An 'oop' code here again....

---
+++ AirConditioner.java
@@ -0,0 +1,33 @@
+public class AirConditioner {
+    private boolean isOn;
+    private String productName;
+    private int temperature;
+
+    public static void main(String[] args){
+    }
+
+    public String AirConditioner(String on){
+        return on;
+    }
+    public void decreaseTemperature(int number){}
+
+    public String getProductName(String LG){
+        return LG;
+    }
+
+    public int getTemperature(int number){
+        return number;
+    }
+
+    public void increaseTemperature(int number){}
+
+    public boolean isOn(){
+        return true;
+    }
+
+    public void setOn(){}
+
+    public void setProductName(String LG ){}
+
+    public void setTemperature(int number){}
+}
