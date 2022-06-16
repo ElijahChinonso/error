fbde9e4c317b73dbd6473f1bbe13b27c05e8456b
Elijah Chinonso <107347041+ElijahChinonso@users.noreply.github.com>
2022-06-16 02:35:59 +0100

    Add files via upload
    A clean oop: check out
    p 2

---
+++ Maiin.java
@@ -0,0 +1,17 @@
+public class Maiin {
+
+    public static void main(String[] args){
+        Headphone nigeriaIsABad = new Headphone();
+        System.out.println(nigeriaIsABad.powerOn());
+        System.out.println(nigeriaIsABad.powerOff());
+        System.out.println(nigeriaIsABad.playPauseOn());
+        System.out.println(nigeriaIsABad.playPauseOff());
+        System.out.println(nigeriaIsABad.microChipOn());
+        System.out.println(nigeriaIsABad.redBlackOn());
+        System.out.println(nigeriaIsABad.skipForward());
+        System.out.println(nigeriaIsABad.skipBackward());
+        System.out.println(nigeriaIsABad.volumeUp());
+        System.out.println(nigeriaIsABad.volumeDown());
+        System.out.println(nigeriaIsABad.redBlackOn());
+    }
+}
