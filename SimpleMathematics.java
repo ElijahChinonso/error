e574200c6b696a41f52355665687e49784dfebde
Elijah Chinonso <107347041+ElijahChinonso@users.noreply.github.com>
2022-06-18 15:12:09 +0100

    Add files via upload
    simple and short

---
+++ SimpleMathematics.java
@@ -0,0 +1,16 @@
+public class SimpleMathematics {
+	
+	private int firstNumber;
+	private int secondNumber;
+
+	public int addTwoNumbers(int twoNumbers){
+		return twoNumbers;
+	}
+			
+	public int addTwoNumbers (int firstNumber, int secondNumber){
+	System.out.println("The result is: " +(firstNumber = secondNumber));
+	
+		return firstNumber + secondNumber;
+	}
+
+}
