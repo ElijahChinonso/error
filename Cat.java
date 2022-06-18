844f4f341698cd08dd0790d65895beb151a89859
Elijah Chinonso <107347041+ElijahChinonso@users.noreply.github.com>
2022-06-18 13:24:29 +0100

    Add files via upload
    This took me while thou,
    relate it to Dog.java.

---
+++ Cat.java
@@ -0,0 +1,14 @@
+public class Cat {
+	private String name;
+
+	public Cat(){}
+
+	public String setCatName(String jerry){
+		return jerry;
+	}
+
+
+	public static void main(String[] args){
+	System.out.println("jerry");
+	}
+}
