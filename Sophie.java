65e795c9d7448b5f9451267c55265b987d552f1c
Elijah Chinonso <107347041+ElijahChinonso@users.noreply.github.com>
2022-06-18 13:47:32 +0100

    Add files via upload
    You could try this also, let's impact each other.

---
+++ Sophie.java
@@ -0,0 +1,13 @@
+public class Sophie {
+	private String love;
+
+	public Sophie(){}
+
+	public String setSophieLove(String Jack){
+		return Jack;
+	}
+
+	public static void main(String[] args){
+	System.out.println("Sophie love Jack");
+	}
+}
