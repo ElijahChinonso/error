d56179a3328569f595a1c4fd377caa916f6938f9
Elijah Chinonso <107347041+ElijahChinonso@users.noreply.github.com>
2022-06-19 19:48:21 +0100

    Add files via upload
    The method

---
+++ HumanId.java
@@ -0,0 +1,23 @@
+public class HumanId {
+	
+	private String birth;
+	private int age;
+	private String school;
+	private int growOld;
+
+	public String birth(String newBorn){
+		return newBorn;
+	}
+
+	public void age(int number){
+		this.age = number;
+	}
+
+	public String school(String theJoy){
+		return theJoy;
+	}
+
+	public void growOld(int oldAge){
+		this.growOld = oldAge;
+	}
+} 
