2038d4f2ff10dc4a0b34a3cf94ac3cf6ac690d5c
Elijah Chinonso <107347041+ElijahChinonso@users.noreply.github.com>
2022-06-19 19:45:56 +0100

    Add files via upload
    The method.....

---
+++ Human.java
@@ -0,0 +1,21 @@
+public class Human {
+	
+	private String birth;
+	private int age;
+	private String school;
+	private int growOld;
+
+	public String birth(String newBorn){
+		return newBorn;
+	}
+
+	public void age(){
+	}
+
+	public String school(String theJoy){
+		return theJoy;
+	}
+
+	public void growOld(){
+	}
+} 
