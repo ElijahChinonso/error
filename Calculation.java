ddc2dfcc55433fa9db8967aacfb7a492abeed4be
Elijah Chinonso <107347041+ElijahChinonso@users.noreply.github.com>
2022-06-20 16:32:03 +0100

    Add files via upload
    Main class

---
+++ Calculation.java
@@ -0,0 +1,19 @@
+public class Calculation {
+	
+
+	public int addNumber(int num5, int num4){
+		return num5 + num4;
+	}
+
+	public int subtractNumber(int num3, int num6){
+		return num3 - num6;
+	}
+
+	public int divideNumber(int num2, int num7){
+		return num2 / num7;
+	}
+
+	public int multiplyNumber(int num9, int num3){
+		return num9 * num3;
+	}
+}	
