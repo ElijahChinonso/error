3ce86b83a2bd94c5a4da1b2021dc09cd237d5752
Elijah Chinonso <107347041+ElijahChinonso@users.noreply.github.com>
2022-06-21 01:09:14 +0100

    Add files via upload
    UML diagram of this code was so tricky, I spent time doing this...

---
+++ Rainbow.java
@@ -0,0 +1,39 @@
+public class Rainbow {
+
+	private String red;
+	private String blue;	
+	private String green;
+
+	public String updateRedColor(){
+		return red;
+	}
+
+	public void setBlue(String gBlue){
+		this.blue = gBlue;
+	}
+
+	public String editBlue(){
+		return blue;
+	}
+
+	public String getRed(){
+		return red;
+	}
+
+	public String setBlue(){
+		return blue;
+	}
+
+	public String editGreen(){
+		return green;
+	}
+
+	public Rainbow(){}
+
+	public Rainbow(String red, String blue){
+	}
+
+	public Rainbow(String red, String blue, String green){
+	}
+} 
+	
